3f2e9c5ebbe5581c7abbcdc0e2dc6809830acd80
Bglaws <glaws.brian@gmail.com>
2021-12-30 14:16:27 -0500

    completed Assignment-0 10

--- Assignment-0/10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ Assignment-0/10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,6 +1,26 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+	let tally = 0;
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] == start) {
+			tally++;
+			i++;
+			for (let j = i; j < nums.length; j++) {
+				if (nums[j] == end) {
+					tally++
+					break;
+				}
+				tally++;
+			}
+			break;
+		}
+	}	
+
+	return tally;
 }
 
 // Do not edit this line;
 module.exports = sumOfNumsWithinARange;
+
+// for (let j = i; nums[j] != end || j < nums.length; j++) {
+// 	tally++;
+// }
