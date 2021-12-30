86de2d41f3ce7dc306b06220c387ba5284c57801
Bglaws <glaws.brian@gmail.com>
2021-12-29 20:58:32 -0500

    completed Assignment-0 05

--- Assignment-0/05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ Assignment-0/05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,11 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+	let tally = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (typeof arr[i] == "boolean" || typeof arr[i] == "string") {
+			tally++;
+		}
+	}
+	return tally;
 }
 
 // Do not edit this line;
