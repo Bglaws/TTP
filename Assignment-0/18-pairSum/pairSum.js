2c042bb7d3f15cb3459b8700661f0370fc11a35a
Bglaws <glaws.brian@gmail.com>
2022-01-02 22:34:09 -0500

    completed Assignment-0 18

--- Assignment-0/18-pairSum/pairSum.js
+++ Assignment-0/18-pairSum/pairSum.js
@@ -1,5 +1,17 @@
 function pairSum(nums, target) {
-  // Insert code here;
+	let hash = {}
+	if (nums.length < 2) {
+		throw 'length of nums is less than 2';
+	}	
+	for (let i in nums) {
+		let addend = target - nums[i];
+
+		if (addend in hash) {
+			return true;
+		}
+	hash[nums[i]] = i;
+	}
+	return false;
 }
 
 // Do not edit this line;
