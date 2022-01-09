5887f513b7e833f6c4a16af4786bed1f1c3ae48f
Brian Glaws <glaws.brian@gmail.com>
2022-01-09 18:19:51 -0500

    progress on assignment 2

---
+++ Assignment-2/myFunctions.mjs
@@ -0,0 +1,53 @@
+function myEach(callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i], i, this)
+    }
+}
+// myEach test
+const arr = [1,2,3,4,5];
+arr.myEach(element => console.log(element))
+
+function myMap() {
+
+}
+
+function myFilter() {
+
+}
+
+function mySome() {
+
+} 
+
+function myEvery() {
+
+}
+
+function myReduce() {
+
+}
+
+function myIncludes() {
+
+}
+
+function myIndexOf() {
+
+}
+
+function myPush() {
+
+}
+
+function myUnshift() {
+
+}
+
+function grabKeys() {
+
+}
+
+function grabValues() {
+    
+}
+
